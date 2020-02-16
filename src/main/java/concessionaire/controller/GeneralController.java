c539f9507de08472daa77da8fa4f4afa1229b5be
hernan232 <hdvanegasm@gmail.com>
2020-02-16 12:13:00 -0500

    integration ontology and example

--- src/main/java/concessionaire/controller/GeneralController.java
+++ src/main/java/concessionaire/controller/GeneralController.java
@@ -5,25 +5,60 @@
  */
 package concessionaire.controller;
 
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.rdf.model.InfModel;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.RDFWriter;
+import org.apache.jena.reasoner.ReasonerRegistry;
+
 /**
  *
  * @author Admin
  */
 public class GeneralController {
-    
+
+    public ResultSet executeQueryToIntegration(String queryString) {
+        Model model = ModelFactory.createDefaultModel();
+        model.read("https://sriw-trabajo1-ontologies.herokuapp.com/ontologies/integration_ontology.owl", "RDF/XML");
+        InfModel inferenceModel = ModelFactory.createInfModel(ReasonerRegistry.getOWLReasoner(), model);
+
+        RDFWriter writer = model.getWriter();
+        String querys;
+
+        Query query = QueryFactory.create(queryString);
+        QueryExecution qexec = QueryExecutionFactory.create(query, inferenceModel);
+        
+        ResultSet results = qexec.execSelect();
+        return results;
+    }
+
+    public ResultSet executeQueryToEndPoint(String queryString, String endpoint) {
+        Query query = QueryFactory.create(queryString);
+        QueryExecution queryExec = QueryExecutionFactory.sparqlService(endpoint, query);
+
+        ResultSet resultSet = queryExec.execSelect();
+        return resultSet;
+    }
+
     public void getClasses() {
 
     }
 
     public void getAttributeFromClass(String entity) {
-        
+
     }
-    
+
     public void getInstancesFromClass(String entity, boolean indirectQuery, String filter) {
 
     }
-    
+
     public void getAttributesFromInstance(String instance) {
-        
+
     }
 }
--- src/main/java/concessionaire/controller/SparqlQuery.java
+++ src/main/java/concessionaire/controller/SparqlQuery.java
@@ -10,5 +10,19 @@ package concessionaire.controller;
  * @author user
  */
 public class SparqlQuery {
-    
+
+    public static String getCarsQuery(String entity, String graph) {
+        if (graph.equals("DEFAULT")) {
+            return "SELECT ?car\n"
+                + "WHERE {\n"
+                + "	?car a <" + entity + "> .\n"
+                + "}";
+        } else {
+            return "SELECT ?car\n"
+                + "FROM <"+ graph +">\n"
+                + "WHERE {\n"
+                + "	?car a <" + entity + ">.\n"
+                + "}";
+        }
+    }
 }
